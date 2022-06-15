5fa02247aef850fbb405a7f6fc7e7b4dd711b2cd
satyajeet <satyajeet@satyajeets-MacB.lan>
2022-06-15 02:00:21 -0400

    updated the main to illsutrate shard counts and changed the sharding toml file

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ var (
 	dbLocation = flag.String("db-location", "", "The path to the bolt db database")
 	httpAddr   = flag.String("http-addr", "127.0.0.1:8080", "HTTP host and port")
 	configFile = flag.String("config-file", "sharding.toml", "Config file for static sharding")
+	shard      = flag.String("shard", "", "The name of the shard for data")
 )
 
 func parseFlags() {
@@ -22,6 +23,10 @@ func parseFlags() {
 
 	if *dbLocation == "" {
 		log.Fatalf("Must provide db-location")
+	}
+
+	if *shard == "" {
+		log.Fatalf("Must provide shard")
 	}
 
 }
@@ -34,6 +39,27 @@ func main() {
 	if _, err := toml.DecodeFile(*configFile, &c); err != nil {
 		log.Fatalf("toml.DecodeFile(%q): %v", *configFile, err)
 	}
+
+	var shardCount int
+	var shardIdx int = -1
+
+	var addrs = make(map[int]string)
+
+	for _, s := range c.Shards {
+		addrs[s.Idx] = s.Address
+
+		if s.Idx+1 > shardCount {
+			shardCount = s.Idx + 1
+		}
+		if s.Name == *shard {
+			shardIdx = s.Idx
+		}
+	}
+
+	if shardIdx < 0 {
+		log.Fatalf("Shard %q was not found", *shard)
+	}
+	log.Printf("Shard count is %d, current shard: %d", shardCount, shardIdx)
 
 	log.Printf("%#v", &c)
 
